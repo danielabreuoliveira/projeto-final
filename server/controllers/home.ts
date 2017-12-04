eb222411ccc21eb4ae1cc2e04e2ddcbf7927b7b6
daniel <danieloliveirabsi@gmail.com>
2017-12-04 01:22:33 -0200

    criado tela home

--- server/controllers/home.ts
+++ server/controllers/home.ts
@@ -1,6 +1,9 @@
 import * as mongoose from 'mongoose'
 const userModel = mongoose.model('User');
 const produtoModel = mongoose.model('Produto');
+const fornecedorModel = mongoose.model('Fornecedor');
+const clienteModel = mongoose.model('Cliente');
+const vendaModel = mongoose.model('Venda');
 
 export default class HomeCtrl {
   
@@ -19,16 +22,41 @@ export default class HomeCtrl {
         console.log(response);
         console.log(homeInfos);
     })
+
     .then(produtoModel.count()
      .then(response => {
          homeInfos.quantProdutos = response
         console.log(response);
         console.log(homeInfos);
     })
+
+    .then(fornecedorModel.count()
     .then(response => {
+        homeInfos.quantFornecedores = response
+       console.log(response);
+       console.log(homeInfos);
+   })
+
+   .then(clienteModel.count()
+   .then(response => {
+       homeInfos.quantClientes = response
+      console.log(response);
+      console.log(homeInfos);
+  }))
+
+  .then(vendaModel.count()
+  .then(response => {
+      homeInfos.quantVendar = response
+     console.log(response);
+     console.log(homeInfos);
+  }) 
+  
+  .then(response => {
     console.log(homeInfos);
         res.json(homeInfos);
-     })
+        })
+        )
+      )
     )
   }
 }
